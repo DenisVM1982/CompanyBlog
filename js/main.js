5cf5f9be78630c049683df938d620e2c513fafff
Artur <denisvalerievichmayorov@gmail.com>
2022-06-11 16:12:50 +0300

    thrird commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 function warningClose() {
     document.getElementById("warning").style.visibility = "hidden";
 }
+
+function sandwichMenuOpen() {
+    console.log("visible ");
+    if(document.getElementById("nav-sadnwich_menu").style.visibility == "hidden") {
+        document.getElementById("nav-sadnwich_menu").style.visibility = "visible";
+    } else if (document.getElementById("nav-sadnwich_menu").style.visibility == "visible") {
+        document.getElementById("nav-sadnwich_menu").style.visibility = "hidden";
+    } else document.getElementById("nav-sadnwich_menu").style.visibility = "visible" ;   
+}
+
+function displayWindowSize() {
+    document.getElementById("nav-sadnwich_menu").style.visibility = "hidden";
+}
+
+window.addEventListener("resize", displayWindowSize);
+
+
+
+
+
